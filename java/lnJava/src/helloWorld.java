2b11794f53d5e2699b908420646cfe75acd9cca8
unknown <eyifang@CN00200907.ericsson.se>
2017-10-18 13:34:00 +0800

    add java

---
+++ java/lnJava/src/helloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by eyifang on 2017/10/18.
+ */
+import java.lang.System;
+
+public class helloWorld {
+
+    public static void main (String[] args){
+        System.out.println("hello");
+    }
+}
