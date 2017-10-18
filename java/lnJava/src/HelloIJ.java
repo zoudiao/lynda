9dfe7539bb1237408378b2a562f6c8ee4197fd05
Yi Fang <silver@Yis-MacBook-Pro.local>
2017-10-18 22:57:18 +0800

    add basic

--- java/lnJava/src/HelloIJ.java
+++ java/lnJava/src/HelloIJ.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by eyifang on 2017/10/18.
  */
@@ -5,7 +7,29 @@ public class HelloIJ {
 
     public static void main(String[] args){
 
-        System.out.println("hello world");
+        //primite
+
+
+    //arrays
+        double[] prices = new double[5];
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("enter 5 prices:");
+
+        double total=0 ;
+
+        for (int i=0;i<prices.length;i++){
+            prices[i] = in.nextDouble();
+            total += prices[i];
+
+        }
+
+        System.out.println(total);
+
+    //data conversion
+
+
 
     }
 }
